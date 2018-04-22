fb338271518984f2ddd7cafc6fb786337b65d9b4
Strelicja <peritus@iwonajaniak.pl>
2018-04-22 15:05:16 +0200

    porfolio

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+
+
+const form = document.querySelector("input[type='submit']");
+const $form = document.querySelector('form');
+
+
+$form.addEventListener("submit", function submit(e) {
+	e.preventDefault();
+	const formObject = new FormData($form);
+	const map = new Map(formObject);
+	const formMessage = map.get('email');
+	function displayMessage(formMessage) {
+		alert(formMessage);
+	};
+	displayMessage(`Jeśli wiesz jak wygląda ten znak back-tick, wyślij maila na adres: ${formMessage} `);
+	
+});
