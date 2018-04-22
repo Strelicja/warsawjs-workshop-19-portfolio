23b6eea707dc451acc38bc3c433e625e031fc513
Strelicja <peritus@iwonajaniak.pl>
2018-04-22 17:19:58 +0200

    changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,19 @@
 
 
+const form = document.querySelector("input[type='submit']");
 const $form = document.querySelector('form');
-function displayMessage(formMessage) {
-		alert(formMessage);
-	};
+
 
 $form.addEventListener("submit", function submit(e) {
 	e.preventDefault();
 	const formObject = new FormData($form);
 	const map = new Map(formObject);
 	const formMessage = map.get('email');
-	
+	function displayMessage(formMessage) {
+		alert(formMessage);
+	};
 	displayMessage(`Jeśli wiesz jak wygląda ten znak back-tick, wyślij maila na adres: ${formMessage} `);
 	
 });
 
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 1;
-var slideInterval = setInterval(nextSlide,2000);
 
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
