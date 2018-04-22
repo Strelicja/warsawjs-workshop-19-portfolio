538816e06d5b28660878ca6c85fd153ac4e7875a
Strelicja <peritus@iwonajaniak.pl>
2018-04-22 16:45:00 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,12 @@ $form.addEventListener("submit", function submit(e) {
 	
 });
 
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 1;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
