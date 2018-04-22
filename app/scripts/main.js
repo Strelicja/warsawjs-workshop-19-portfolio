26eeca208cbe7d0778c5a5671aa477304c808727
Strelicja <peritus@iwonajaniak.pl>
2018-04-22 17:28:44 +0200

    js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,16 @@
 
 
-const form = document.querySelector("input[type='submit']");
 const $form = document.querySelector('form');
-
+function displayMessage(formMessage) {
+		alert(formMessage);
+	};
 
 $form.addEventListener("submit", function submit(e) {
 	e.preventDefault();
 	const formObject = new FormData($form);
 	const map = new Map(formObject);
 	const formMessage = map.get('email');
-	function displayMessage(formMessage) {
-		alert(formMessage);
-	};
+	
 	displayMessage(`Jeśli wiesz jak wygląda ten znak back-tick, wyślij maila na adres: ${formMessage} `);
 	
 });
